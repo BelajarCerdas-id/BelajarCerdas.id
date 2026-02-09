adb80edd9172422a205a18b957cf78bf81be6502
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-02-09 11:09:29 +0700

    style(sidebar): update header logo bc in mentor

--- resources/views/components/sidebar_beranda.blade.php
+++ resources/views/components/sidebar_beranda.blade.php
@@ -252,11 +252,11 @@
 @elseif(Auth::user()->role === 'Mentor')
     <!--- Sidebar Beranda for dekstop ---->
     <aside class="sidebar-beranda hidden md:block">
-        <div class="logo_details flex items-center justify-center">
-            <a href="{{ route('beranda') }}">
-                <img src="{{ asset('image/logoBC.png') }}" alt="" class="w-2/5">
-            </a>
-        </div>
+        <a href="{{ route('beranda') }}">
+            <div class="logo_details flex items-center justify-center">
+                <img src="{{ asset('image/logo-bc/white-logo-bc.svg') }}" alt="" class="!w-50 h-24">
+            </div>
+        </a>
         <ul class="nav-list">
             <li class="list-menu">
                 <a href="/beranda">
@@ -397,7 +397,7 @@
         <div class="flex items-center h-full">
             <i class="fas fa-bars text-2xl relative top-1 cursor-pointer text-white" onclick="togglePopup()"></i>
             <a href="{{ route('beranda') }}">
-                <img src="{{ asset('image/logoBC.png') }}" alt="" class="w-[65px] ml-4">
+                <img src="{{ asset('image/logo-bc/white-logo-bc.svg') }}" alt="" class="w-[110px] ml-4">
             </a>
         </div>
         <div class="flex items-center gap-8 text-2xl relative top-1">
@@ -440,7 +440,7 @@
         <div class="overlay-sidebar-phone"></div>
         <div class="content-sidebar-phone">
             <header class="w-full h-20 bg-[--color-second] flex items-center justify-between pl-2 pr-6">
-                <img src="{{ asset('image/logoBC.png') }}" alt="" class="w-[60px]">
+                <img src="{{ asset('image/logo-bc/white-logo-bc.svg') }}" alt="" class="w-[100px]">
                 <i class="fas fa-xmark text-2xl text-white cursor-pointer" onclick="togglePopup()"></i>
             </header>
             <main>
